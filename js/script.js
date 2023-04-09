b93c1bb7ec6ec1683f63e4e31cbdd0bcb5998008
Mustafa Reisoğlu <m.reisogluuu@gmail.com>
2023-04-09 15:14:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// ================= typing animation =========================
+var tyed = new Typed(".typing",{
+    strings:["","Full Stack","Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
